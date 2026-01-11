9b576239f77eaf43a1d6905b2118b114c5eb9f4b
JardaZach <k1attack@seznam.cz>
2026-01-11 17:26:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const produkty = [
+  {
+    id: 1,
+    nazev: "Notebook",
+    cena: 24999,
+    skladem: true,
+  },
+  {
+    id: 2,
+    nazev: "Sluchátka",
+    cena: 1999,
+    skladem: false,
+  },
+  {
+    id: 3,
+    nazev: "Mobilní telefon",
+    cena: 13999,
+    skladem: true,
+  },
+]
+
+const produktyDiv = document.getElementById("produkty")
+
+produkty.forEach(produkt => {
+  const mujprodukt = document.createElement("div")
+
+  const nazevProduktu = document.createElement("h2")
+  nazev.textContent = produkt.nazev
+
+  const cenaProduktu = document.createElement("p")
+  cena.textContent = `Cena: ${produkt.cena} Kč`
+
+  const naSklade = document.createElement("p")
+  if (produkt.skladem) {
+    naSklade.textContent = "Tento produkt je skladem"
+    naSklade.style.color = "green"
+  } else {
+    naSklade.textContent = "Tento produkt není skladem"
+    naSklade.style.color = "red"
+  }
+
+  mujprodukt.appendChild(nazevProduktu)
+  mujprodukt.appendChild(cenaProduktu)
+  mujprodukt.appendChild(naSklade)
+
+  produktyDiv.appendChild(mujprodukt)
+})
+
